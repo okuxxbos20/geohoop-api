f0634a2ac80b34d50798fce4b9e7e63ccb8bd3a5
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-11-19 15:32:30 +0900

    enable to UPDATE

--- app/routes/v1/user.ts
+++ app/routes/v1/user.ts
@@ -44,48 +44,33 @@ router.post('/', (req: Req, res: Res) => {
 })
 
 // UPDATE ONE USER
-router.patch('/:id', async(req: Req, res: Res) => {
-  const User = new UserModel()
+router.put('/:id', (req: Req, res: Res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'bio']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
   const uid = req.params.id
-
+  const body = {
+    name: req.body.name,
+    email: req.body.email,
+    bio: req.body.bio
+  }
   if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' })
   }
 
-  try {
-    const user = await User.findByIdAndUpdate(
-      uid,
-      req.body,
-      { new: true, runValidators: true }
-    )
-
+  UserModel.findByIdAndUpdate(uid, body).then((user: any) => {
     if (!user) {
-      return res.status(404).send()
+      res.send('error')
+    } else {
+      user.save((err: Error) => {
+        if (err){
+          res.send(err)
+        } else {
+          res.json({ message: 'Success To Update!' })
+        }
+      })
     }
-    res.send(user)
-  } catch (err) {
-    res.status(500).send(err)
-  }
-
-  // UserModel.findByIdAndUpdate(uid).then((err: Error, user: any) => {
-  //   if (err) {
-  //     res.send(err)
-  //   } else {
-  //     user.name = req.body.name
-  //     user.email = req.body.email
-  //     user.bio = req.body.bio
-  //     user.save((err: Error) => {
-  //       if (err){
-  //         res.send(err)
-  //       } else {
-  //         res.json({ message: 'Success To Update!' })
-  //       }
-  //     })
-  //   }
-  // })
+  })
 })
 
 module.exports = router
